898ad921aaab3822c4bc344370ee0cb0fc78371b
DanielSantamariaAlvarez <47115911+DanielSantamariaAlvarez@users.noreply.github.com>
2022-04-04 16:37:46 -0500

    avances carga

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class Banner extends Component {
   render() {
-    return <div>Banner</div>;
+    return (<>
+
+            <nav>
+                <div className="logo">Museo de arte moderno</div>
+                <ul>
+                    <li className="bold subrayado"><u>Museo</u></li>
+                    <li>Artistas</li>
+                    <li>Movimientos</li>
+                </ul>
+            </nav>
+    
+    </>);
   }
 }
 
--- src/components/List-museums.js
+++ src/components/List-museums.js
@@ -1,9 +1,60 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import Museum from './Museum'
+import MuseumDetail from "./Museum-detail";
 
-class ListMuseums extends Component {
-  render() {
-    return <div>ListMuseums</div>;
-  }
+function ListMuseums () {
+
+    const [museos, setMuseos] = useState([]);
+    const [museo, setMuseo] = useState([]);
+    let centinela = true;
+
+    useEffect(() => {
+        fetch("https://back-museums-uniandes.herokuapp.com/api/museums")
+        .then((result) => result.json())
+        .then((lista) => setMuseos(lista));
+    }, []);
+
+    const vaciar = () => {
+        centinela = false
+        setMuseo([])
+    }
+    const seleccion = (museoId) => {
+        let encontrado = museos.find((museo) => museo.id === museoId);
+        setMuseo(encontrado);
+      };
+    
+    return (
+        < div className="container">
+            <p className="">
+                <span>Home</span> {" > "} <span onClick={vaciar}>Museos</span>
+                {centinela && (
+                <>
+                    {" > "}
+                    <span> {museo.name} </span>
+                </>
+                )}
+            </p>
+
+            <div className="header">
+                {centinela ? "MUSEOS" : museo.name + ": Obras principales"}
+            </div>
+            {centinela?(
+                <div className="museum-list row">
+          {museos.map((mus) => (
+                <div className="col-md-3">
+                    <MuseumDetail
+                        museum={mus}
+                        elegirMuseo={seleccion}
+                        key={mus.id}
+                    ></MuseumDetail>
+                </div>
+          ))}
+        </div>
+            ):(
+                <Museum museum={museo}></Museum>
+            )}
+        </div>
+        )
 }
 
 export default ListMuseums;
